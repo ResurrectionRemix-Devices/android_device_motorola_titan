d98720b590b79b2862af3d02d329e92094a7f2f6
Mohammad Rais Al Islam <raisul2010.5396@gmail.com>
2018-06-05 19:37:45 +0600

    bringup RR

--- rr.mk
+++ rr.mk
@@ -1,14 +1,11 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
-# Inherit some common toxyc stuff.
-$(call inherit-product, vendor/toxyc/config/common_full_phone.mk)
+# Inherit some common RR stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := toxyc_titan
+PRODUCT_NAME := rr_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
-
-#export
-TOXYC_BUILD_TYPE=OFFICIAL
