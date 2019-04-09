2fe3eb2b33e242962b24f32abf69590b5e3e526b
Dan Santos <dansantosbe@gmail.com>
2019-04-08 23:54:48 -0300

    titan: Move properties to rr_titan.mk

--- rr_titan.mk
+++ rr_titan.mk
@@ -1,9 +1,38 @@
-$(call inherit-product, device/motorola/titan/full_titan.mk)
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Release name
+PRODUCT_RELEASE_NAME := titan
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from titan device
+$(call inherit-product, device/motorola/titan/device.mk)
 
 # Inherit some common RR stuff.
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
-PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := rr_titan
+# Set bootanimation
 TARGET_BOOT_ANIMATION_RES := 720
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := titan
+PRODUCT_NAME := rr_titan
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := titan
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_SHIPPING_API_LEVEL := 19
+
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
